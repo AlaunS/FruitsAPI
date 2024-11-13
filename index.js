6e35482dba5cf68337447be7cdb2cdcecc3c5909
Alan <mendezalau@gmail.com>
2024-11-13 13:31:58 -0600

    Despliegue

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 (0, dotenv_1.config)();
 // Creamos el servidor de express
 const app = (0, express_1.default)();
+// Inicializamos cors
+app.use((0, cors_1.default)());
 app.use(express_1.default.static('public')); // Obtenemos el directorio publico
 app.use(express_1.default.json()); // Lectura y parseo del body
-// Parseo del body
-app.use(express_1.default.json());
 // Rutas
 app.use('/image', require('./routes/images.js'));
 // Escuchar peticiones
--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
 
 import { config } from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 config();
 
 // Creamos el servidor de express
 const app = express();
 
+// Inicializamos cors
+app.use(cors());
+
 app.use(express.static('public'));      // Obtenemos el directorio publico
 app.use(express.json());                // Lectura y parseo del body
-
-// Parseo del body
-app.use(express.json());
 
 // Rutas
 app.use('/image', require('./routes/images.js'));
