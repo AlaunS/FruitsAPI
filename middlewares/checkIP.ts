c3b3e8aa5cebc3a29cd62c9f8c98542018f1f303
Alan <mendezalau@gmail.com>
2024-11-14 14:15:44 -0600

    Despliegue sin errores

--- middlewares/checkIP.ts
+++ middlewares/checkIP.ts
@@ -5,6 +5,7 @@ export const CheckSameIP = async(req: Request, res: Response, next: NextFunction
     const clientIp = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.ip)?.toString().replace("::ffff:", "");
     const newUser = await UserModel.findOne({ user: req.params.user });
 
+    console.log(clientIp)
     if (newUser ){
         if (newUser.deviceIP !== clientIp){
             return res.status(500).json({
