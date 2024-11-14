d1975b663502482a9990628524fcbad4b5e367c8
Alan <mendezalau@gmail.com>
2024-11-14 14:14:14 -0600

    Despliegue sin errores

--- middlewares/checkIP.ts
+++ middlewares/checkIP.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { UserModel } from "../models/User";
 
 export const CheckSameIP = async(req: Request, res: Response, next: NextFunction): Promise<any> => {
-    const clientIp = (req.ip || req.socket.remoteAddress)?.replace("::ffff:", "");
+    const clientIp = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.ip)?.toString().replace("::ffff:", "");
     const newUser = await UserModel.findOne({ user: req.params.user });
 
     if (newUser ){
