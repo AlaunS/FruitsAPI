ad3c1676cf356fe2eaf365e8baa0a9bd49ea96c6
Alan <mendezalau@gmail.com>
2024-11-19 07:21:35 -0600

    CAmbios

--- routes/images.js
+++ routes/images.js
@@ -2,9 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const images_1 = require("../controllers/images");
-const checkIP_1 = require("../middlewares/IP/checkIP");
 const router = (0, express_1.Router)();
-router.get('/random/:user', checkIP_1.CheckSameIP, images_1.GetRandomImage); // Obtenemos una imagen aleatoria
-router.get('/all/:user', checkIP_1.CheckSameIP, images_1.GetAllImages); // Obtener todas las imagenes del servicio
-router.get('/:name/:user', checkIP_1.CheckSameIP, images_1.GetImage); // Obtenemos la imagen introducida por el usuario
+router.get('/random', [], images_1.GetRandomImage); // Obtenemos una imagen aleatoria
+router.get('/all', [], images_1.GetAllImages); // Obtener todas las imagenes del servicio
+router.get('/:name', [], images_1.GetImage); // Obtenemos la imagen introducida por el usuario
 module.exports = router;
