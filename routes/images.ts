9ccf54f251e89373b30e86e5d4bac70ed88d4b5a
Alan <mendezalau@gmail.com>
2024-11-19 07:14:09 -0600

    Cambios

--- routes/images.ts
+++ routes/images.ts
@@ -4,8 +4,8 @@ import { GetAllImages, GetImage, GetRandomImage } from "../controllers/images";
 import { CheckSameIP } from "../middlewares/IP/checkIP";
 
 const router = Router();
-router.get('/random/:user', CheckSameIP, GetRandomImage)   // Obtenemos una imagen aleatoria
-router.get('/all/:user', CheckSameIP, GetAllImages);       // Obtener todas las imagenes del servicio
-router.get('/:name/:user', CheckSameIP, GetImage);         // Obtenemos la imagen introducida por el usuario
+router.get('/random', [], GetRandomImage)   // Obtenemos una imagen aleatoria
+router.get('/all', [], GetAllImages);       // Obtener todas las imagenes del servicio
+router.get('/:name', [], GetImage);         // Obtenemos la imagen introducida por el usuario
 
 module.exports = router;
